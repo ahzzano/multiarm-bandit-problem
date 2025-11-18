6ae3ff6c31db259065ece24a578b3328a80eea20
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-11-18 19:34:06 +0800

    feat: add Arm() class

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class Arm:
+    name: str
+    probability: float = 0.5
+
+    @classmethod
+    def new(cls, name: str, probability):
+        return Arm(name, probability)
+
 def main():
+    arms = [
+        Arm.new("A", 0.5),
+        Arm.new("B", 0.3),
+        Arm.new("C", 0.2),
+    ]
+
     print("Hello from multiarm-bandit-problem!")
 
 
